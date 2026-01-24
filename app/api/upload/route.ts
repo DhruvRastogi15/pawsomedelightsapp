30b60bc6049b1a21c0845c45f26327cd5f9960e2
Dhruv Rastogi <dhruvrastogi@Dhruvs-MacBook-Air.local>
2026-01-24 15:23:19 +0530

    add cloudinary sdk

---
+++ app/api/upload/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        { message: "Image is required" },
+        { status: 400 }
+      );
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    const uploadResult: any = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({ folder: "products" }, (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        })
+        .end(buffer);
+    });
+
+    return NextResponse.json({
+      imageUrl: uploadResult.secure_url,
+    });
+
+  } catch (err: any) {
+    console.error("UPLOAD ERROR:", err);
+    return NextResponse.json(
+      { message: "Upload failed" },
+      { status: 500 }
+    );
+  }
+}
